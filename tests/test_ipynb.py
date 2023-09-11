91596339ae4faad7e17b6d596f81bf6f20a6b246
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-09-11 03:57:35 +0200

    Fix imports for 3.12 (NothingChanged)

--- tests/test_ipynb.py
+++ tests/test_ipynb.py
@@ -9,15 +9,9 @@ import pytest
 from _pytest.monkeypatch import MonkeyPatch
 from click.testing import CliRunner
 
-from black import (
-    Mode,
-    NothingChanged,
-    format_cell,
-    format_file_contents,
-    format_file_in_place,
-    main,
-)
+from black import Mode, format_cell, format_file_contents, format_file_in_place, main
 from black.handle_ipynb_magics import jupyter_dependencies_are_installed
+from black.report import NothingChanged
 from tests.util import DATA_DIR, get_case_path, read_jupyter_notebook
 
 with contextlib.suppress(ModuleNotFoundError):
